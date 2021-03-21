9c9cf5e5e1cc3fb6154c76f535cff1d1005d543c
mmoscovics <mmoscovics@yahoo.com>
2021-03-21 13:32:42 -0400

    [Update] fix exit

--- 0x0C-nqueens/0-nqueens.py
+++ 0x0C-nqueens/0-nqueens.py
@@ -55,13 +55,13 @@ def main():
 
     if len(sys.argv) != 2:
         print("Usage: nqueens N")
-        exit(1)
+        sys.exit(1)
     if not sys.argv[1].isdigit():
         print("N must be a number")
-        exit(1)
+        sys.exit(1)
     if n < 4:
         print("N must be at least 4")
-        exit(1)
+        sys.exit(1)
     solve_nqueens(n, board, 0)
 
 
