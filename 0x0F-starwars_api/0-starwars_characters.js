d15148f7f5bb3a58e00db4864d3df7f63f568860
Mitchell <mmoscovics@yahoo.com>
2021-04-08 19:40:20 -0400

    [Update] recreate file

---
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+const request = require('request');
+const films = {
+  url: 'https://swapi-api.hbtn.io/api/films/' + process.argv[2]
+};
+
+request(films, async function (err, res, body) {
+  if (err) {
+    console.log(err);
+  }
+  const json = JSON.parse(body);
+  for (const character of json.characters) {
+    await new Promise(function (resolve, reject) {
+      request(character, function (err, res, body) {
+        if (err) {
+          console.log(err);
+        }
+        console.log(JSON.parse(body).name);
+        resolve();
+      });
+    });
+  }
+});
