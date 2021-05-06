32f05c2913e8d25f90e41c0bb9fab111b700c9c7
Mitchell <mmoscovics@yahoo.com>
2021-05-06 17:32:57 -0400

    [Update] specific words

--- 0x13-count_it/0-count.py
+++ 0x13-count_it/0-count.py
@@ -12,19 +12,20 @@ def count_words(subreddit, word_list, after=None, words={}):
 
     if len(words) == 0:
         for word in word_list:
+            word = word.lower()
             words[word] = 0
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     user_agent = {"User-Agent": "Hello There 1.0"}
     response = requests.get(url, headers=user_agent, allow_redirects=False,
                             params={"after": after})
     if response.status_code != 200:
-        print()
+        return None
     children = response.json()["data"]["children"]
     for child in children:
         for key, value in words.items():
             titles = child["data"]["title"]
             word_count = titles.lower().split().count(key)
-            if key in titles:
+            if key in titles.lower():
                 words[key] += word_count
     after = response.json()["data"]["after"]
     if after is None:
